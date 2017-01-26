ade84674527966136ac8a75111bfb6de507ea2ff
Trevor Von Seggern <tvoman2@gmail.com>
2017-01-25 20:34:45 -0700

    Slight ui changes.

--- src/client/app/pages/elo/game/item/detail/controller.ts
+++ src/client/app/pages/elo/game/item/detail/controller.ts
@@ -4,20 +4,35 @@ import IScope = angular.IScope;
 import {ItemService} from '../service';
 import {BaseDetailItemController} from "../../../../../component/baseItem/item/detail/controller";
 import {eloDefinition} from "../../../eloDefinition";
+import {UserFactory} from "../../../../userManagement/user/factory";
 
 export class controller extends BaseDetailItemController {
-	static controllerName:string = eloDefinition.gameDetail.controllerName;
-	static $inject:any[]         = [
+	static controllerName: string = eloDefinition.gameDetail.controllerName;
+	static $inject: any[] = [
 		'$scope',
 		'$window',
 		'$stateParams',
-		ItemService.serviceName
+		ItemService.serviceName,
+		UserFactory.factoryName,
 	];
 
-	constructor($scope:IScope,
-	            $window:IWindowService,
-	            $stateParams,
-	            public itemService:ItemService) {
+	processGame() {
+		this.loading = true;
+		this.userFactory.httpServerCall('/api/elo/game/process/' + this.item._id, 'get', undefined, (data) => {
+			this.loading = false;
+			if (data.data.error)
+				console.log(data.data.message);
+		}, (error) => {
+			console.log(error);
+			this.loading = false;
+		});
+	}
+
+	constructor($scope: IScope,
+				$window: IWindowService,
+				$stateParams,
+				public itemService: ItemService,
+				public userFactory: UserFactory) {
 		super($scope, $window, $stateParams, itemService, '/#/game');
 	}
 }
--- src/server/api/elo/game/controller.ts
+++ src/server/api/elo/game/controller.ts
@@ -1,6 +1,6 @@
 // Created by trevor on 12/31/16.
 import express = require('express');
-import {getSize, newItem, saveItem, getList, getOneItem, deleteItem} from "./functions";
+import {getSize, newItem, saveItem, getList, getOneItem, deleteItem, processEloGame} from "./functions";
 import {isLoggedIn} from "../../auth/authStrategies";
 
 export let GameRouter = express.Router();
@@ -12,6 +12,7 @@ GameRouter.route('/')
 	.post(isLoggedIn, newItem);
 
 GameRouter.route('/size').get(getSize);
+GameRouter.route('/process/:_id').get(processEloGame);
 
 GameRouter.route('/:_id')
 	.get(isLoggedIn, getOneItem)

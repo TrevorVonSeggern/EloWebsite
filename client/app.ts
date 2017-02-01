07a4064557c0d4634febb6645821057110d3f8a4
Trevor Von Seggern <tvoman2@gmail.com>
2017-02-01 03:02:05 -0700

    Added nav bar.

--- client/app.ts
+++ client/app.ts
@@ -1,11 +1,42 @@
+import {DefinitionRouter, definition} from "./app/Definition";
 /**
  * Created by trevor on 7/8/2016.
  */
 // my ui components:
 require('web-input-text/module');
 require('web-datetime/module');
+require('web-nav/module');
 
-let app = angular.module('elo-app', ['input-text-module', 'datetime-module']);
+let app = angular.module('elo-app', [
+	'input-text-module',
+	'datetime-module',
+	'nav-module'
+]);
+
+// let router = angular.module('router-module', ['ui.router']);
+// app.config([
+// 	'$stateProvider',
+// 	'$compileProvider',
+// 	function ($stateProvider, $compileProvider) {
+// 		$compileProvider.debugInfoEnabled(true); // keep the dom insync with under the hood stuff. For server pre-gen
+//
+// 		// loadRouter($stateProvider); // add states for each page
+//
+// 		// $stateProvider.state('logout', {
+// 		// 	url: '/logout',
+// 		// 	controller: LogoutController,
+// 		// 	template: ''
+// 		// });
+// 		// $stateProvider.state('googleOAuth2', {
+// 		// 	url: '/state=:state&access_token=:accessToken&token_type=:tokenType&expires_in=:expires',
+// 		// 	template: 'Loading data from google\'s servers...',
+// 		// 	controller: GoogleOAuthController
+// 		// });
+// 		$stateProvider.state('otherwise', new DefinitionRouter('', definition.login));
+//
+// 	}
+// ]);
+
 
 app.controller('ctrl', ['$scope', function ($scope) {
 	$scope.asdfvar = 'asdfvar var';
--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,18 @@
 let angular: any;
 
 let mod = angular.module('app', ['elo-app']);
-mod.controller('appctrl', function () {
-	this.value = '';
-});
+mod.controller('ctrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+	let vm = $scope;
+	vm.loggedIn = true;
+	let users = ['Administrator', 'User'];
+	vm.menu = [
+		{name: 'Game', url: 'game', baseName: 'game', requireLogin: true, users: users},
+		{name: 'Event', url: 'event', baseName: 'event', requireLogin: true, users: users},
+		{name: 'Match', url: 'match', baseName: 'match', requireLogin: true, users: users},
+		{name: 'Team', url: 'team', baseName: 'team', requireLogin: true, users: users},
+		{name: 'Player', url: 'player', baseName: 'player', requireLogin: true, users: users},
+	];
+	vm.root = $rootScope;
+	vm.userRole = 'Guest';
+	vm.userless = false;
+}]);
